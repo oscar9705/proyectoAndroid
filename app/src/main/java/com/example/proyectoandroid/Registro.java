eee9b110698a4d0fc8f5e59646b3a57a72497fb3
oscar salazar <salazaroscar9705@gmail.com>
2020-11-21 08:32:04 -0500

    registro de los datos del usuario en la colección users

--- app/src/main/java/com/example/proyectoandroid/Registro.java
+++ app/src/main/java/com/example/proyectoandroid/Registro.java
@@ -3,6 +3,8 @@ package com.example.proyectoandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import com.example.proyectoandroid.entity.Student;
 import com.example.proyectoandroid.entity.User;
@@ -15,11 +17,16 @@ import java.util.Map;
 
 public class Registro extends AppCompatActivity {
     FirebaseFirestore db = FirebaseFirestore.getInstance();
-
+    private EditText cedula, nombre, correo, telefono, password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+        cedula =(EditText)findViewById(R.id.cedulaEditText);
+        nombre =(EditText)findViewById(R.id.nameEditText);
+        correo =(EditText)findViewById(R.id.correoEditText);
+        telefono =(EditText)findViewById(R.id.celularEditText);
+        password =(EditText)findViewById(R.id.editTextPassword);
         cargarDatosFirebase();
     }
     public void cargarDatosFirebase(){
@@ -38,5 +45,39 @@ public class Registro extends AppCompatActivity {
         user.put("alumnos",studentList);
 
         db.collection("users").document("salazaroscar9705@gmail.com").set(user);
+    }
+    public void registrarUser(View v){
+        User user = crearUser();
+        Map<String, Object> userMap = hashMapFirebase(user);
+        db.collection("users").document(user.getCorreo()).set(userMap);
+
+    }
+    public User crearUser(){
+        User user = new User();
+        user.setCedula(Integer.parseInt(cedula.getText().toString()));
+        user.setNombre(nombre.getText().toString());
+        user.setCorreo(correo.getText().toString());
+        user.setTelefono(telefono.getText().toString());
+        user.setPassword(password.getText().toString());
+
+        return user;
+    }
+    public Map<String, Object>  hashMapFirebase(User user){
+        List<Student> studentList = new ArrayList<>();
+        Map<String, Object> userMap =new HashMap<>();
+        userMap.put("cedula",user.getCedula());
+        userMap.put("nombre",user.getNombre());
+        userMap.put("correo",user.getCorreo());
+        userMap.put("telefono",user.getTelefono());
+        userMap.put("password",user.getPassword());
+        userMap.put("alumnos",studentList);
+        return userMap;
+    }
+    public void limpiarCampos(){
+        nombre.setText("");
+        cedula.setText("");
+        correo.setText("");
+        telefono.setText("");
+        password.setText("");
     }
 }
